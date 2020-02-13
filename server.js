3097591615a2ebe0b4c8c95c0bce88183410c623
Paxton Sharp <Paxtonsharp22@gmail.com>
2020-02-12 20:41:45 -0600

    data is pushed to terminal

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const passport = require('./config/passport');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const bcrypt = require('bcrypt')
 
 app.set('view-engine', 'ejs')
-
+app.use(express.urlencoded({ extended: false }))
 const users = [];
+
 //passport
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'bob' })
@@ -28,11 +30,29 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
+app.post('/login', (req, res) => {
+  
+})
 
 app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
 
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users)
+})
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
