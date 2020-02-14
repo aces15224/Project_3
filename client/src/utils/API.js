622fda8f92107830ae706b8bc1c1c5cac365cf17
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-13 21:35:48 -0600

    CRUD Functions
    Added.

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all tasks
+  getTasks: function() {
+    return axios.get("/api/tasks");
+  },
+  // Gets the task with the given id
+  getTask: function(id) {
+    return axios.get("/api/tasks/" + id);
+  },
+  // Deletes the task with the given id
+  deleteTask: function(id) {
+    return axios.delete("/api/tasks/" + id);
+  },
+  // Saves a task to the database
+  saveTask: function(postData) {
+    return axios.post("/api/tasks", postData);
+  }
+};
