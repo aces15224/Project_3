d94211effd809efa072e8b0e4b1e4ab08127aa65
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-16 20:28:24 -0600

    Moved stuff around.  Body Parser.  Morgan logger.
    And cleaned up code a bit.

--- server.js
+++ server.js
@@ -1,97 +1,91 @@
-// const dotenv = require('dotenv').config()
-// require('dotenv').config()
-
 const path = require('path');
 const express = require('express');
-
-
-// const helmet = require('helmet');
-// const cors = require('cors');
 const session = require('express-session');
+const mongoose = require('mongoose');
+const logger = require('morgan');
 
-const db = require('./models');
+// const db = require('./models');
+const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt')
+
 const routes = require('./routes');
 const passport = require('./config/passport');
-// const corsOptions = require('./config/cors.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const bcrypt = require('bcrypt')
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-const users = [];
 
-//passport
-app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'bob' })
-})
 
-app.get('/login', (req, res) => {
-  res.render('login.ejs')
-})
-app.post('/login', (req, res) => {
+
+// app.set('view-engine', 'ejs')
+// app.use(express.urlencoded({ extended: false }))
+// const users = [];
+
+// //passport
+// app.get('/', (req, res) => {
+//   res.render('index.ejs', { name: 'bob' })
+// })
+
+// app.get('/login', (req, res) => {
+//   res.render('login.ejs')
+// })
+// app.post('/login', (req, res) => {
   
-})
+// })
 
-app.get('/register', (req, res) => {
-  res.render('register.ejs')
-})
+// app.get('/register', (req, res) => {
+//   res.render('register.ejs')
+// })
 
-app.post('/register', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-  console.log(users)
-})
+// app.post('/register', async (req, res) => {
+//   try {
+//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//     users.push({
+//       id: Date.now().toString(),
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: hashedPassword
+//     })
+//     res.redirect('/login')
+//   } catch {
+//     res.redirect('/register')
+//   }
+//   console.log(users)
+// })
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// app.use(helmet());
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(session({ secret: 'TBD', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(cors(corsOptions));
 
+// Mongoose Connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds035517.mlab.com:35517/heroku_fj6klq23";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+ 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// Add routes, both API and view
+
+
 app.use(routes);
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
 }
 
-// Dynamically force schema refresh only for 'test'
-// const FORCE_SCHEMA = process.env.NODE_ENV === 'test';
-
-// db.sequelize
-//   .authenticate()
-//   .then(() => {
-//     db.sequelize.sync({ force: FORCE_SCHEMA }).then(() => {
-//       console.log(`🌎 ==> API server now on port ${PORT}!`); // eslint-disable-line no-console
-//       app.emit('appStarted');
-//     });
-//   }).catch(console.error); // eslint-disable-line no-console
-app.listen(PORT, function () {
-  console.log("Server is running on Port: " + PORT);
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
 });
-
-module.exports = app;
+// module.exports = app;
