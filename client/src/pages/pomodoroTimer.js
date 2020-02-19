4bcc50aa3aa2e03f2445e3b3d5e7a03ad1725393
Harris <hglane@crimson.ua.edu>
2020-02-18 22:50:06 -0600

    Working on Pomodoro Timer

--- client/src/pages/pomodoroTimer.js
+++ client/src/pages/pomodoroTimer.js
@@ -1,23 +1,38 @@
 import React from "react";
 
-function PomodoroTimer() {
-    return (
-      <div>
-        <h1>PomodoroTimer</h1>
-        <p>
-          Switch to code below once component is finished
-        </p>
-      </div>
-    );
+const Header = () => <h1>Pomodoro Clock</h1>
+
+const SetTimer = ({ type, value }) => (
+  <div class='SetTimer'>
+    <div id={`${type}-label`}>{`${type} Length`}</div>
+    <div class="SetTimer-controls">
+      <button id={`${type}-decrement`}>&darr;</button>
+      <div id={`${type}-length`}>{value}</div>
+      <button id={`${type}-increment`}>&uarr;</button>
+    </div>
+  </div>
+)
+
+
+class PomodoroTimer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      breakValue: 5,
+      sessionValue: 25
+    }
   }
-  
-  export default PomodoroTimer;
-  
-
-
-
-// class PomodoroTimer extends React.Component {
-    
-// }
+   render() {
+     return(
+       <div>
+         <Header/>
+         <div class="settings">
+           <SetTimer type='Break' value={this.state.breakValue}/>
+           <SetTimer type='Session' value={this.state.sessionValue}/>
+         </div>
+       </div>
+     )
+   } 
+}
 
 // export default  PomodoroTimer;
