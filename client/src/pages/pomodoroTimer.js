e6944fde32e2d520a5f0e7c8879dd496927bfc4d
Harris <hglane@crimson.ua.edu>
2020-02-19 19:19:52 -0600

    More work with timer

--- client/src/pages/pomodoroTimer.js
+++ client/src/pages/pomodoroTimer.js
@@ -5,11 +5,18 @@ const Header = () => <h1>Pomodoro Clock</h1>
 const SetTimer = ({ type, value }) => (
   <div class='SetTimer'>
     <div id={`${type}-label`}>{`${type} Length`}</div>
-    <div class="SetTimer-controls">
+    <div class='SetTimer-controls'>
       <button id={`${type}-decrement`}>&darr;</button>
       <div id={`${type}-length`}>{value}</div>
       <button id={`${type}-increment`}>&uarr;</button>
     </div>
+  </div>
+)
+
+const Timer = ({ mode, time }) => (
+  <div class='Timer'>
+    <h1>{mode === 'session' ? 'Session' : 'Break'}</h1>
+    <h1>{time}</h1>
   </div>
 )
 
@@ -19,17 +26,20 @@ class PomodoroTimer extends React.Component {
     super(props)
     this.state = {
       breakValue: 5,
-      sessionValue: 25
+      sessionValue: 25,
+      mode: 'session',
+      time: 25 * 60 * 1000
     }
   }
    render() {
      return(
        <div>
          <Header/>
-         <div class="settings">
+         <div class='settings'>
            <SetTimer type='Break' value={this.state.breakValue}/>
            <SetTimer type='Session' value={this.state.sessionValue}/>
          </div>
+         <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
        </div>
      )
    } 
