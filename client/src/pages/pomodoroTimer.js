f2599c4cb1d0c031652a2725efa018690ae25e0c
Harris <hglane@crimson.ua.edu>
2020-02-22 11:15:22 -0600

    Almost there!

--- client/src/pages/pomodoroTimer.js
+++ client/src/pages/pomodoroTimer.js
@@ -5,28 +5,28 @@ const Header = () => <h1>Pomodoro Clock</h1>
 
 const SetTimer = ({ type, value, handleClick }) => (
   <div class='SetTimer'>
-    <div id={`${type}-label`}>{`${type} Length`}</div>
+    <div id={`${type}-label`}>{type == 'session' ? 'Session ' : 'Break '}Length</div>
     <div class='SetTimer-controls'>
       <button id={`${type}-decrement`} onClick={() => handleClick(false, `${type}Value`)}>&darr;</button>
-      <div id={`${type}-length`}>{value}</div>
+      <h1 id={`${type}-length`}>{value}</h1>
       <button id={`${type}-increment`} onClick={() => handleClick(true, `${type}Value`)}>&uarr;</button>
     </div>
   </div>
 )
 
-const Timer = ({ mode, time }) => (
+const Timer = ({ mode, time}) => (
   <div class='Timer'>
     <h1 id='timer-label'>{mode === 'session' ? 'Session' : 'Break'}</h1>
     <h1 id='time-left'>{time}</h1>
   </div>
 )
 
-const Controls = ({ active }) => (
+const Controls = ({ active, handleReset, handlePlayPause  }) => (
   <div class='Controls'>
-    <button id='start_stop'>
+    <button id='start_stop' onClick={handlePlayPause}>
       { active ? <span>&#10074;&#10074;</span> : <span>&#9658;</span> }
     </button>
-    <button id='reset'>&#8635;</button>
+    <button id='reset' onClick={handleReset}>&#8635;</button>
   </div>
 )
 
@@ -43,7 +43,24 @@ class PomodoroTimer extends React.Component {
   }
 
   handleSetTimers = (inc, type) => {
+    if(this.state[type] ==  60 && inc) return
+    if(this.state[type] == 1 && !inc) return
     this.setState({ [type]: this.state[type] + (inc ? 1 : -1) })
+  }
+
+  handleReset = () => {
+    this.setState({ breakValue: 5, sessionValue: 25, time: 25 * 60 * 1000 })
+    clearInterval(this.pomodoro)
+  }
+
+  handlePlayPause = () => {
+    if(this.state.active) {
+      clearInterval(this.pomodoro)
+      this.setState({ active: false })
+    } else {
+      this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000 }), 1000)
+      this.setState({ active: true })
+    }
   }
 
    render() {
@@ -55,7 +72,10 @@ class PomodoroTimer extends React.Component {
            <SetTimer type='session' value={this.state.sessionValue} handleClick={this.handleSetTimers}/>
          </div>
          <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
-         <Controls active={this.state.active}/>
+         <Controls 
+          active={this.state.active} 
+          handlePlayPause={this.handlePlayPause}
+          handleReset={this.handleReset}/>
        </div>
      )
    } 
