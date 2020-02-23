0213ce451b6ee1e4f350a8bc26677ecbba0c8690
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-23 12:21:38 -0600

    Added a container for easier styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
-// import Wrapper from "./components/Wrapper";
-// import TaskBank from "./components/TaskBank";
-// import TaskList from "./components/TaskList";
 import CalendarPage from "./pages/calendar";
-// import Dashboard from "./pages/dashboard";
 import taskBank from "./pages/TaskBankHome";
 import PomodoroTimer from "./pages/pomodoroTimer";
 import UserStats from "./pages/userStats";
 import './App.css';
-import Header from "./components/Header";
 
 class App extends Component {
  
   render() {
     return (
       <Router>
-        <div class= "container">
-          <Header />
-            <NavBar />
+        <div>
+          <NavBar />
+            <div className="container" id="mainDisplay">
+              <Route exact path="/" component={CalendarPage} />
+              <Route exact path="/tasks" component={taskBank} />
+              <Route exact path="/timer" component={PomodoroTimer} />
+              <Route exact path="/stats" component={UserStats} />
+            </div>  
             
-            <Route exact path="/" component={CalendarPage} />
-            {/* <Route exact path="/dashboard" component={Dashboard} /> */}
-            <Route exact path="/tasks" component={taskBank} />
-            <Route exact path="/timer" component={PomodoroTimer} />
-            <Route exact path="/stats" component={UserStats} />
 
           
         </div>
