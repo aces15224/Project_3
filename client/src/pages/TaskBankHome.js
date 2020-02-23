f03108602ee93e5f37ee51234e2fc8959c0698d6
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-23 15:13:18 -0600

    Card container for Task List
    CSS'n.

--- client/src/pages/TaskBankHome.js
+++ client/src/pages/TaskBankHome.js
@@ -1,23 +1,31 @@
- 
 import React from 'react';
 import TaskBank from "../components/TaskBank/TaskBank";
 import Form from "../components/Form/Form";
 
-
-
 const TaskBankHome = () => { 
-  
- return (
-      <div>
+
+  return (
+    <div className="card" id="taskCard">
+      <div className="card-header">
         <h1>Task Bank</h1>
-        <Form />
-        <TaskBank/>
       </div>
-    ); 
+      <div className="card-body">
+        <div className="row">
+          <div className="col-8">
+            <div id="taskBank">
+              <TaskBank/>  
+            </div>
+          </div>
+          <div className="col-4">
+            <Form /> 
+          </div>
+        </div>       
+      </div>
+    </div>  
+  ); 
 }
 
   export default TaskBankHome;
-
 
 
 
