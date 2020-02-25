c917bab4f9d3fd842cb755e760f1f3d4a6b1c06f
Harris <hglane@crimson.ua.edu>
2020-02-24 20:47:30 -0600

    Working register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,13 +1,57 @@
-  
- import React from "react";
- 
-
- function registerPage() {
+import React, { useRef } from 'react';
+// import API from "../../utils/API";
+const RegisterPage = (props) => {
+    // const [userForm, setUserForm] = useState({
+    //     email: "",
+    //     password: "",
+    // });
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const firstname = useRef();
+    const lastname = useRef();
+    // useEffect(() => {
+    //     console.log();
+    // }, []);
+    //Form Submit Function
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        // alert("works")
+        // creFormusing form data & dropdown
+        var userData = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            firstname: firstname.current.value,
+            lastname: lastname.current.value
+        }
+        console.log("THIS IS THE STATE", userData)
+    };
     return (
-      <div>
-        <h1>Register Info</h1>
-      </div>
-    );
-  }
+        <form className="form-group" onSubmit={handleFormSubmit} >
+            <div className="form-group">
+                <label className="taskAdd"><h3>User Info</h3></label>
+                <br></br>
+                <input className="col-12 form-control" ref={emailRef} type="text"
+                    name="email"
+                    placeholder="input email"
+                />
+                <input className="col-12 form-control" ref={passwordRef} type="text"
+                    name="password"
+                    placeholder="input password"
+                />
+                 <input className="col-12 form-control" ref={firstname} type="text"
+                    name="firstname"
+                    placeholder="input first name"
+                />
+                 <input className="col-12 form-control" ref={lastname} type="text"
+                    name="lastname"
+                    placeholder="input last name"
+                />
+            </div>
+            <button type="submit" className="submitBtn btn btn-primary">
+                Submit
+            </button>
+        </form>
+    )
+}
 
-export default registerPage;
+export default RegisterPage;
