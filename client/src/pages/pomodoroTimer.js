68f2a0bf8d0e221dc288a34eb2a70f598c74a6a2
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-28 17:15:30 -0600

    Add files via upload

--- client/src/pages/pomodoroTimer.js
+++ client/src/pages/pomodoroTimer.js
@@ -2,10 +2,29 @@ import React from "react";
 import moment from "moment";
 import './pomodoroTimer.css'
 
-const Header = () => <h1>Pomodoro Clock</h1>
+const quotes = 
+["Either you run the day or the day runs you.", 
+"Whether you think you can, or you think you can’t – you’re right.",
+"Don’t wish it were easier. Wish you were better.",
+"Do the hard jobs first. The easy jobs will take care of themselves.",
+"The future depends on what you do today.", 
+"The man who moves a mountain begins by carrying away small stones.",
+"Things may come to those who wait, but only the things left by those who hustle.",
+"To think too long about doing a thing often becomes its undoing.",
+"You will never plough a field if you only turn it over in your mind.",
+"Don't wait. The time will never be just right.",
+"You don’t have to see the whole staircase, just take the first step.",
+"You don’t have to see the whole staircase, just take the first step.",
+"Happiness is the real sense of fulfillment that comes from hard work.",
+"If you want to make an easy job seem mighty hard, just keep putting off doing it.",
+"It does not matter how slowly you go, so long as you do not stop.",
+"Amateurs sit and wait for inspiration, the rest of us just get up and go to work."
+]
+
+const Header = () => <h1 className="display-4" id="pomClock">Pomodoro Clock</h1>
 const SetTimer = ({ type, value, handleClick }) => (
   <div className='SetTimer'>
-    <div id={`${type}-label`}>{type === 'session' ? 'Session ' : 'Break '}Length</div>
+    <div className="timerText" id={`${type}-label`}>{type === 'session' ? 'Session ' : 'Break '}Length</div>
     <div className='SetTimer-controls'>
       <button id={`${type}-decrement`} onClick={() => handleClick(false, `${type}Value`)}>&darr;</button>
       <h1 id={`${type}-length`}>{value}</h1>
@@ -39,7 +58,8 @@ class PomodoroTimer extends React.Component {
       mode: 'session',
       time: 25 * 60 * 1000,
       active: false,
-      touched: false
+      touched: false,
+      quote:"Time to Get Motivated"
     }
   }
 
@@ -87,30 +107,41 @@ class PomodoroTimer extends React.Component {
           time: this.state.sessionValue * 60 * 1000, 
           touched: true,
           active: true }, () => this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000}), 1000))
+        setInterval(()=>{
+          var currentQuote=quotes[Math.floor(Math.random() * quotes.length)]
+          this.setState({quote:currentQuote})
+        }, 100000);
       }
-      
     }
   }
 
    render() {
      return(
        <div>
-         <Header/>
-         <div className='settings'>
-           <SetTimer type='break' value={this.state.breakValue} handleClick={this.handleSetTimers}/>
-           <SetTimer type='session' value={this.state.sessionValue} handleClick={this.handleSetTimers}/>
-         </div>
-         <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
-         <Controls 
-          active={this.state.active} 
-          handlePlayPause={this.handlePlayPause}
-          handleReset={this.handleReset}/>
-          <audio 
-            id='beep' 
-            src='https://s3-us-west-1.amazonaws.com/benjaminadk/Data+synth+beep+high+and+sweet.mp3' 
-            ref={el => this.audio = el}
-            >
-          </audio>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4"><Header/></h1>
+            <p className="lead">{this.state.quote}</p>
+          </div>
+        </div>
+          <div class="card">
+            <div class="card-body">
+              <div className='settings'>
+                <SetTimer type='break' value={this.state.breakValue} handleClick={this.handleSetTimers}/>
+                <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
+                <SetTimer type='session' value={this.state.sessionValue} handleClick={this.handleSetTimers}/>
+                </div>  
+                <Controls 
+                  active={this.state.active} 
+                  handlePlayPause={this.handlePlayPause}
+                  handleReset={this.handleReset}/>
+                  <audio 
+                    id='beep' 
+                    src='https://s3-us-west-1.amazonaws.com/benjaminadk/Data+synth+beep+high+and+sweet.mp3' 
+                    ref={el => this.audio = el}
+                />
+            </div>
+          </div>  
        </div>
      )
    } 
