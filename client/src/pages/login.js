b264149136f57acb63e80d50b688116c71ad3ef5
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-28 21:06:39 -0600

    Add files via upload

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -20,7 +20,7 @@ const LoginForm = (props) => {
           })
           .then(function (response) {
             console.log(response);
-            window.location.replace("/calendar")
+            window.location.replace("/home")
             //redirect
           })
           .catch(function (error) {
@@ -52,32 +52,10 @@ const LoginForm = (props) => {
                     <button type="submit" className="submitBtn btn btn-primary">
                         Submit
                     </button>
+                    <a href="/register" class="btn btn-success">Register</a>
                 </form>
             </div>
         </div>
-        // <div class="jumbotron jumbotron-fluid">
-        //     <div class="container">
-        //         <h1 class="display-4">User Login!</h1>
-        //         <p class="lead">Welcome to the Motivate Me App!  Please Login!</p>
-        //     </div>
-        // </div>
-        // <form className="form-group" onSubmit={handleFormSubmit} >
-        //     <div className="form-group">
-        //         <label className="taskAdd"><h3>User Info</h3></label>
-        //         <br></br>
-        //         <input className="col-12 form-control" ref={emailRef} type="text"
-        //             name="email"
-        //             placeholder="input email"
-        //         />
-        //         <input className="col-12 form-control" ref={passwordRef} type="text"
-        //             name="password"
-        //             placeholder="input password"
-        //         />
-        //     </div>
-        //     <button type="submit" className="submitBtn btn btn-primary">
-        //         Submit
-        //     </button>
-        // </form>
     )
 }
 
