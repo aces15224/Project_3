008dbe274d93acbfe008433396f2151a6c123bec
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-28 18:31:28 -0600

    Add files via upload

--- client/src/components/TaskBank/TaskBank.js
+++ client/src/components/TaskBank/TaskBank.js
@@ -1,17 +1,3 @@
-// import React from 'react'
-// // import axios from 'axios'
-// import TaskList from '../TaskList/TaskList'
-
-// class TaskBank extends React.Component {
-//   render () {
-//     const { task } = this.props;  
-//     return <TaskList task={task}/>     
-// }};
-
-// export default TaskBank;
-//////////Take out code above upon completion/////////////////
-
-
 import React, {useState} from 'react'
 import TaskList from '../TaskList/TaskList'
 
@@ -28,4 +14,3 @@ const TaskBank = () => {
 
 
 export default TaskBank;
-
